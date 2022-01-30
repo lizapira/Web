d6d8d046b1d56fa9e2579b164f6b5b5ea5e22733
lizapira <lp47657@ubt-uni.net>
2022-01-30 14:41:15 +0100

    admin

---
+++ Web/confirm_room2.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Confirm Booking</title>
+</head>
+<style>
+	body {
+	  
+	  margin: 0;
+	  background-image:linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url("images/image4.jpg") ;
+      color: peru;
+      height: 2vh;
+      background-size: cover;
+      background-position: center;
+	}
+	table {
+		font-size: 22px;
+	}
+	p {
+		font-size: 24px;
+	}
+	#td1
+	{
+		background-color: saddlebrown;
+		color: white;
+		border: 10px;
+		margin-top: -10px;
+		padding: 10px;
+		text-align: center;
+	}
+	td {
+		text-align: left;
+	}
+	th {
+		font-weight: bold;
+		text-align: left;
+	}
+	ul {
+	  	list-style-type: none;
+	  	margin: 0;
+	  	padding: 0;
+	  	width: 22%;
+	  	font-size: 24px;
+	  	background-color: peru;
+	  	text-decoration: none;
+	  	position: fixed;
+	  	height: 100%;
+	  	overflow: auto;
+	}
+	li {
+		color: white;
+	}
+	a {
+	  	display: block;
+	  	color: white;
+	  	padding: 8px 16px;
+	  	text-decoration: none;
+	}
+	a:active {
+	  	background-color: saddlebrown;
+	  	color: white;
+	  	text-decoration: underline;		
+	}
+	a:hover {
+	  	background-color: peru;
+	  	color: white;
+	  	text-decoration: underline;
+	}
+</style>
+<body>
+	<?php
+		$conn = new mysqli("localhost","root","", "iwp");
+		if($conn->connect_error)
+		{
+			die("Connection failed: ".$conn->connect_error);
+		}
+		$sql = "SELECT * from temp_session";
+		$result=mysqli_query($conn, $sql);
+		$row=mysqli_fetch_row($result); ?>
+	<table style="width: 100%;">
+		<tr>
+			<td id="td1" style="padding: 10px; font-size: 48px;">Golden <p style="color: peru; font-size: 48px; display: inline;">Hotel</p></td>
+			<td id="td1" style="font-size: 25px; text-align: right;">Hello, <?php echo $row[2]; ?></td>
+		</tr>
+	</table>
+	<ul>
+		<li><a href="user_view.php">My Info</a></li>
+		<li><a href="bookroom.php">Book A Room</a></li>
+		<li><a href="user_room_status.php">Show Booking Status</a></li>
+		<li><a href="user_payment.php">Payment</a></li>
+		<li><a href="user_booking_history.php">Booking History</a></li>
+		<li><a href="index.php">Logout</a></li>
+	</ul>
+	<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+		<p>Sorry! All rooms are full in this type.<br>Wait for the rooms to get empty.</p>
+		<P><br>Click Next to get redirected to home.</P>
+		<a href="admin_view.php" style="width: 10%; text-align: center; color: black; border: 3px solid #e6b800;">Next</a>
+	</div>
+</body>
+</html>
